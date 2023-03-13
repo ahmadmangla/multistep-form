7fa1b45e9fca70ec18eb6377e3856b3671da7f1f
adilmangla8 <adil.ahmad@ucp.edu.pk>
2023-03-13 22:50:23 +0000

    new changes

---
+++ src/components/Form.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import AddOn from "./AddOn";
+import PersonalInfo from "./PersonalInfo";
+import SelectPlan from "./SelectPlan";
+import Summary from "./Summary";
+
+const Form = () => {
+  const titles = ["YOUR INFO", "SELECT PLAN", "ADD-ONS", "SUMMARY"];
+  const [controlStep, setControlStep] = useState(0);
+  console.log(useState);
+  let i = 0;
+
+  function stepController() {
+    if (titles[controlStep] === "YOUR INFO") {
+      return <PersonalInfo />;
+    } else if (titles[controlStep] === "SELECT PLAN") {
+      return <SelectPlan />;
+    } else if (titles[controlStep] === "ADD-ONS") {
+      return <AddOn />;
+    } else if (titles[controlStep] === "SUMMARY") {
+      return <Summary />;
+    }
+  }
+
+  function handleForwardClick() {
+    if (controlStep !== titles.length - 1) {
+      setControlStep(controlStep + 1);
+    }
+  }
+
+  function handleBackwardClick() {
+    if (controlStep !== 0) {
+      setControlStep(controlStep - 1);
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="side-bar">
+        <div className="step-container">
+          <ul>
+            {titles.map((item) => {
+              i = i + 1;
+              return (
+                <li className="step-item">
+                  <div
+                    className="step-number"
+                    style={
+                      controlStep === i - 1
+                        ? {
+                            backgroundColor: "#bee1fd",
+                            borderColor: "#bee1fd",
+                            color: "var(--primary-color)",
+                          }
+                        : {}
+                    }
+                  >
+                    {i}
+                  </div>
+                  <div className="">
+                    <span>{`STEP ${i}`}</span>
+                    <h4
+                      style={
+                        controlStep === i - 1
+                          ? {
+                              color: "var(--primary-color)",
+                            }
+                          : {}
+                      }
+                    >
+                      {item}
+                    </h4>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="main-form">
+        {stepController()}
+        <div className="buttons">
+          {controlStep !== 0 ? (
+            <button onClick={handleBackwardClick} className="prev-step-button">
+              Prevous Step
+            </button>
+          ) : (
+            ""
+          )}
+
+          <button onClick={handleForwardClick} className="next-step-button">
+            Next Step
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/components/PersonalInfo.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const PersonalInfo = () => {
+  return (
+    <>
+      <div className="form-title">
+        <h1>Personal Info</h1>
+        <p>Please Provide your name, email address and phone number</p>
+      </div>
+      <form action="">
+        <div className="form-item">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" placeholder="e.g Stphen King" />
+        </div>
+        <div className="form-item">
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="e.g Stphenking@gmail.com"
+          />
+        </div>
+        <div className="form-item">
+          <label htmlFor="phone">Phone Number</label>
+          <input type="text" name="phone" placeholder="e.g +1 2334 343434" />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default PersonalInfo;
